a8d196ba45b02c93d72035bbb1f224429b625f11
Cjay1019 <cjay741@gmail.com>
2019-02-17 23:04:58 -0500

    finished some design and html layout

--- public/assets/js/index.js
+++
@@ -1,6 +0,0 @@
-$("body").on("click", ".unthrown", function(event) {
-  var id = $(this).attr("id");
-  $.ajax(`/api/discs/${id}`, { type: "PUT", data: id }).then(function(data) {
-    location.reload();
-  });
-});
